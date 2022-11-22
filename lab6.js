535613d13463c94ece276285caf3c3e43b47c685
Денис Данилов <denyeezy350@gmail.com>
2022-11-22 09:34:29 +0700

    Переводы

---
+++ lab6.js
@@ -0,0 +1,39 @@
+function setRussian() {
+    let name = document.getElementById('student');
+    name.innerHTML = 'Денис Данилов';
+
+    let footer = document.getElementById('footer');
+    footer.innerHTML = 'Факультет бизнеса, ФБИ-02, 3 курс, 2022'
+
+    document.getElementById('your-name').innerHTML = 'Ваше имя';
+    document.getElementById('input-name').placeholder = 'Введите имя';
+
+    document.getElementById('your-surname').innerHTML = 'Ваша фамилия';
+    document.getElementById('input-surname').placeholder = 'Введите фамилию';
+
+    document.getElementById('your-age').innerHTML = 'Ваш возраст';
+    document.getElementById('input-age').placeholder = 'Введите возраст';
+
+    document.getElementById('ok-button').value = 'Ок';
+    document.getElementById('reset-button').value = 'Сброс';
+}
+
+function setEnglish() {
+    let name = document.getElementById('student');
+    name.innerHTML = 'Denis Danilov';
+
+    let footer = document.getElementById('footer');
+    footer.innerHTML = 'Faculty of Business, FBI-02, 3rd year, 2022'
+
+    document.getElementById('your-name').innerHTML = 'Your name';
+    document.getElementById('input-name').placeholder = 'Enter your name';
+
+    document.getElementById('your-surname').innerHTML = 'Your surname';
+    document.getElementById('input-surname').placeholder = 'Enter your surname';
+
+    document.getElementById('your-age').innerHTML = 'Your age';
+    document.getElementById('input-age').placeholder = 'Enter your age';
+
+    document.getElementById('ok-button').value = 'Ok';
+    document.getElementById('reset-button').value = 'Reset';
+}
