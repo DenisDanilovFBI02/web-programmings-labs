49402dc9f43f2e5ec2f0d7f0ef6d432f5ddbabdb
Денис Данилов <denyeezy350@gmail.com>
2022-12-28 21:54:45 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -11,3 +11,16 @@ function click2(button) {
     else
     button.innerHTML = 'Денис';
 }
+
+function click3(button) {
+    if(button.innerHTML == 'Нажми меня')
+    button.innerHTML = 'Ещё раз нажми';
+
+    else if(button.innerHTML == 'Ещё раз нажми')
+    button.innerHTML = 'Ещё';
+
+    else if(button.innerHTML == 'Ещё')
+    button.innerHTML = 'Начать сначала';
+    else
+    button.innerHTML = 'Нажми меня';
+}
