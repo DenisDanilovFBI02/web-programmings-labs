73c815319741cf26aa88c663e72a088fc6f14fe3
Денис Данилов <denyeezy350@gmail.com>
2022-12-28 21:48:25 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -1,6 +1,13 @@
-function clickME(button) {
+function click1(button) {
     if(button.innerHTML == 'Щелчок')
     button.innerHTML = 'Спасибо';
     else 
     button.innerHTML = 'Щелчок';
+}
+
+function click2(button) {
+    if(button.innerHTML == 'Денис')
+    button.innerHTML = 'Данилов';
+    else
+    button.innerHTML = 'Денис';
 }
